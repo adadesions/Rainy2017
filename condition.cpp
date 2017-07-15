971625fe8a4af293269e9102b443305bdc4f9261
adadesions <ada_dev@icloud.com>
2017-07-15 10:35:57 +0700

    Condition file

--- condition.cpp
+++ condition.cpp
@@ -8,10 +8,13 @@ int main() {
   cout << "Enter some number that you think is more than 100: ";
   cin >> input;
   cout << endl;
-
+  cout << "input is : " << input << endl;
   //if-else
-  if( input > a){
+  if( input > a ){
     cout << "Yeah!! input more than 100" << endl;
+  }
+  else if( input == 100 ){
+    cout << "Ok! input is 100" << endl;
   }
   else {
     cout << "Oh!! Try again!!" << endl;
