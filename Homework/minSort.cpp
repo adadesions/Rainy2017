63f251c3c97157f725534a922f2f7271b5d3f06a
adadesions <ada_dev@icloud.com>
2017-08-10 22:51:44 +0700

    HW2.2 donegit add .

---
+++ Homework/minSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int findMin( int data[], int size ){
+  int min = 10000000;
+  int index = 0;
+  for(int i = 0; i < size; ++i){
+    if(min >= data[i]){
+      min = data[i];
+      index = i;
+    }
+  }
+  data[index] = 100000000;
+  return min;
+}
+
+int main() {
+  int data[] = {78, 12, 4, 73, 1405, 267, 3, 9012, 44, 1};
+  int size = sizeof(data)/sizeof(int);
+  int sorted[size];
+
+  for(int i = 0; i < size; ++i){
+    sorted[i] = findMin(data, size);
+    cout << sorted[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
